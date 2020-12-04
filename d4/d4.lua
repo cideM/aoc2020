a34ad71842d1dc2d4357b2cc7e26242ed3fc9384
Florian Beeres <yuuki@protonmail.com>
2020-12-04 17:42:21 +0100

    Improve Lua

--- d4/d4.lua
+++ d4/d4.lua
@@ -5,92 +5,49 @@
 
 local lines = {[1] = {}}
 for line in io.lines() do
-	if line:match("^$") then
-		lines[#lines + 1] = {}
-	end
-
-	for k, v in line:gmatch("(%g+):(%g+)") do
-		lines[#lines][k] = v
-	end
+	if line:match("^$") then lines[#lines + 1] = {} end
+	for k, v in line:gmatch("(%g+):(%g+)") do lines[#lines][k] = v end
 end
 
+local required = { byr = true, iyr = true, eyr = true, hgt = true, hcl = true, ecl = true, pid = true }
 local valid = 0
-for _,line in ipairs(lines) do
-	if line.byr
-		and line.iyr
-		and line.eyr
-		and line.hgt
-		and line.hcl
-		and line.ecl
-		and line.pid
-		then valid = valid + 1
-	end
+for _,l in ipairs(lines) do
+	for k, _ in pairs(required) do if not l[k] then goto skip end end
+	valid = valid + 1
+	::skip::
 end
+
+local rules = {
+	byr = function (v) return tonumber(v) >= 1920 and tonumber(v) <= 2002 end,
+	iyr = function (v) return tonumber(v) >= 2010 and tonumber(v) <= 2020 end,
+	eyr = function (v) return tonumber(v) >= 2020 and tonumber(v) <= 2030 end,
+	hcl = function (v) local h = v:match("#([0-9a-f]+)") return h and #h == 6 end,
+	cid = function (v) return true end,
+	pid = function (v) local h = v:match("%d+") return h and #h == 9 end,
+	ecl = function (v) return v:match("amb") 
+		or v:match("blu")
+		or v:match("brn")
+		or v:match("gry")
+		or v:match("grn")
+		or v:match("hzl")
+		or v:match("oth")
+	end,
+	hgt = function (v)
+		if not v:match("(%d+)cm") and not v:match("(%d+)in") then return false end
+		local h = v:match("(%d+)cm")
+		if h then return (tonumber(h) >=150) and (tonumber(h) <= 193) end
+
+		local h = v:match("(%d+)in")
+		if h then return (tonumber(h) >=59) and (tonumber(h) <= 76) end
+	end,
+}
 
 local valid2 = 0
-for _,line in ipairs(lines) do
-	for k,v in pairs(line) do
-		if not line.byr
-			or not line.iyr
-			or not line.eyr
-			or not line.hgt
-			or not line.hcl
-			or not line.ecl
-			or not line.pid
-			then goto skip
-		end
-
-		if k == "byr" 
-			and (not (tonumber(v) >= 1920) or not (tonumber(v) <= 2002))
-			then goto skip 
-		end
-
-		if k == "iyr" 
-			and (not (tonumber(v) >= 2010) or not (tonumber(v) <= 2020))
-			then goto skip 
-		end
-
-		if k == "eyr" 
-			and (not (tonumber(v) >= 2020) or not (tonumber(v) <= 2030))
-			then goto skip 
-		end
-
-		if k == "hgt" then
-			if not v:match("(%d+)cm") and not v:match("(%d+)in") then goto skip end
-			local h = v:match("(%d+)cm")
-			if h 
-				and (not (tonumber(h) >=150) or not (tonumber(h) <= 193))
-				then goto skip
-			end
-
-			local h = v:match("(%d+)in")
-			if h 
-				and (not (tonumber(h) >= 59) or not (tonumber(h) <= 76))
-				then goto skip
-			end
-		end
-
-		if k == "hcl" then
-		        local d = v:match("#([0-9a-f]+)") 
-			if not d or #d ~= 6 then goto skip end
-		end
-
-		if k == "ecl" 
-			and not v:match("amb") 
-			and not v:match("blu")
-			and not v:match("brn")
-			and not v:match("gry")
-			and not v:match("grn")
-			and not v:match("hzl")
-			and not v:match("oth")
-			then goto skip 
-		end
-
-		if k == "pid" then
-			if #v:match("%d+") ~= 9 then goto skip end
-		end
+for _,l in ipairs(lines) do
+	for k,v in pairs(l) do
+		for r, _ in pairs(required) do if not l[r] then goto skip end end
+		if not rules[k](v) then goto skip end
 	end
-
 	valid2 = valid2 + 1
 	::skip::
 end
