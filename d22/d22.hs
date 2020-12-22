1c62d4142ca3ebe18c677171307af9bd10767982
Florian Beeres <yuuki@protonmail.com>
2020-12-23 00:15:34 +0100

    bit of golf

--- d22/d22.hs
+++ d22/d22.hs
@@ -1,8 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [extra trifecta containers flow])" -i "runghc -Wall"
 
-{-# LANGUAGE LambdaCase #-}
-
 import Data.Foldable (toList)
 import Data.Maybe (isJust)
 import Data.Sequence (Seq (..))
@@ -28,40 +26,30 @@ winner (Empty, x) = Just x
 winner (x, Empty) = Just x
 winner _ = Nothing
 
-playp2 :: Set (Seq Int, Seq Int) -> (Seq Int, Seq Int) -> Either (Seq Int) (Seq Int)
-playp2 _ (Empty, a) = Right a
-playp2 _ (a, Empty) = Left a
-playp2 seen current@(a :<| a', b :<| b')
-  | Set.member current seen = fst current |> Left
-  | Seq.length a' >= a && Seq.length b' >= b =
-    let nextA = Seq.take a a'
-        nextB = Seq.take b b'
-        seen' = Set.insert current seen
-     in case playp2 Set.empty (nextA, nextB) of
-          Left _ -> playp2 seen' (a' Seq.|> a Seq.|> b, b')
-          Right _ -> playp2 seen' (a', b' Seq.|> b Seq.|> a)
-  | otherwise = turn current |> playp2 (Set.insert current seen)
-
 score :: Seq Int -> Int
 score = toList .> reverse .> flip zip [1 ..] .> fmap (uncurry (*)) .> sum
 
 p2 :: (Seq Int, Seq Int) -> String
-p2 =
-  playp2 Set.empty .> \case
-    Left deck -> score deck |> show
-    Right deck -> score deck |> show
+p2 = playp2 Set.empty .> either (score .> show) (score .> show)
+  where
+    playp2 :: Set (Seq Int, Seq Int) -> (Seq Int, Seq Int) -> Either (Seq Int) (Seq Int)
+    playp2 _ (Empty, a) = Right a
+    playp2 _ (a, Empty) = Left a
+    playp2 seen current@(a :<| a', b :<| b')
+      | Set.member current seen = fst current |> Left
+      | Seq.length a' >= a && Seq.length b' >= b =
+        let seen' = Set.insert current seen
+         in case playp2 Set.empty (Seq.take a a', Seq.take b b') of
+              Left _ -> playp2 seen' (a' Seq.|> a Seq.|> b, b')
+              Right _ -> playp2 seen' (a', b' Seq.|> b Seq.|> a)
+      | otherwise = turn current |> playp2 (Set.insert current seen)
 
 p1 :: (Seq Int, Seq Int) -> String
 p1 = until (winner .> isJust) turn .> winner .> fmap score .> show
 
-solve :: String -> String
-solve =
-  parseString parse mempty
-    .> \case
-      Failure e -> show e
-      Success (player1 : player2 : _) ->
-        [p1 (player1, player2), p2 (player1, player2)] |> unlines
-      _ -> error ":("
+solve :: [Seq Int] -> String
+solve (a : b : _) = [p1 (a, b), p2 (a, b)] |> unlines
+solve _ = error ":("
 
 main :: IO ()
-main = interact solve
+main = interact (parseString parse mempty .> foldResult show solve)
